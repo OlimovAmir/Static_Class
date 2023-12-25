8972a9e667aece75292e033b285c4fea71b6c67c
olimov Amir <olimov.amir@mail.ru>
2023-12-25 17:32:36 +0300

    Добавьте файлы проекта.

---
+++ MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class
+{
+    static class MyClass
+    {
+        static MyClass()
+        {
+            
+        }
+
+        static int myVar;
+
+        static int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+        static void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Static_Class.MyClass myClass = new Static_Class.MyClass();
+    }
+}
